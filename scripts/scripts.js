5fc4ec3d314470ef9d33a280242543d4d2b651af
kwinkich <vankord21@gmail.com>
2023-10-11 23:56:18 +0300

    add new files & switch theme and save ls

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,84 +1,105 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
-const swiper = new Swiper('.swiper', {
+// Swiper
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs';
+
+let swiper;
+
+const commonSwiperOptions = {
     on: {
         slideChange: function () {
-        var currentSlide = swiper.activeIndex + 1;
-        document.getElementById('current-slide').textContent = '0' + currentSlide;
+            var currentSlide = swiper.activeIndex + 1;
+            document.getElementById('current-slide').textContent = '0' + currentSlide;
         },
     },
-    // Optional parameters
-    direction: 'vertical',
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-    parallax:true,
+    parallax: true,
+};
+
+const verticalSwiperOptions = {
+    ...commonSwiperOptions,
+    direction: 'vertical',
     mousewheel: {
         mousewheelControl: true,
         eventsTarget: '.usecases__content',
         forceToAxis: true,
         releaseOnEdges: true,
-    }
-});
+    },
+};
 
-const animationApp = document.querySelector('.features-block__app-block');
-const animationExchange = document.querySelector('.features-block__exchange-block');
-const animationStat = document.querySelector('.features-block__stat-block')
-const animationSendCoins = document.querySelector('.features-block__send-coins');
-const animationAnonymous = document.querySelector('.features-block__anonymous');
+const horizontalSwiperOptions = {
+    ...commonSwiperOptions,
+    direction: 'horizontal',
+};
+
+function updateSwiper() {
+    const isDesktop = window.innerWidth >= 1060;
+    const chosenOptions = isDesktop ? verticalSwiperOptions : horizontalSwiperOptions;
+
+    if (swiper) {
+        swiper.destroy(true, true);
+    }
+
+    swiper = new Swiper('.swiper', chosenOptions);
+}
+
+updateSwiper();
+
+window.addEventListener('resize', updateSwiper);
+
+
+// Theme and Animation
 const videoApp = document.getElementById('app-video');
 const videoExchange = document.getElementById('exchange-video');
 const videoStat = document.getElementById('stat-video');
 const videoSendCoins = document.getElementById('send-coins-video');
 const videoAnonymous = document.getElementById('anonymous-video');
 
-animationApp.addEventListener('mouseenter', () => {
-    videoApp.play();
+const animationElements = [
+    { element: document.querySelector('.features-block__app-block'), video: videoApp },
+    { element: document.querySelector('.features-block__exchange-block'), video: videoExchange },
+    { element: document.querySelector('.features-block__stat-block'), video: videoStat },
+    { element: document.querySelector('.features-block__send-coins'), video: videoSendCoins },
+    { element: document.querySelector('.features-block__anonymous'), video: videoAnonymous },
+];
+
+animationElements.forEach(({ element, video }) => {
+    element.addEventListener('mouseenter', () => {
+        video.play();
+    });
+
+    element.addEventListener('mouseleave', () => {
+        video.pause();
+    });
 });
 
-animationApp.addEventListener('mouseleave', () => {
-    videoApp.pause();
-});
+const switchButton = document.getElementById('switch-theme');
 
-animationExchange.addEventListener('mouseenter', () => {
-    videoExchange.play();
-});
-
-animationExchange.addEventListener('mouseleave', () => {
-    videoExchange.pause();
-});
-
-animationStat.addEventListener('mouseenter', () => {
-    videoStat.play();
-});
-
-animationStat.addEventListener('mouseleave', () => {
-    videoStat.pause();
-});
-
-animationSendCoins.addEventListener('mouseenter', () => {
-    videoSendCoins.play();
-});
-
-animationSendCoins.addEventListener('mouseleave', () => {
-    videoSendCoins.pause();
-});
-
-animationAnonymous.addEventListener('mouseenter', () => {
-    videoAnonymous.play();
-});
-
-animationAnonymous.addEventListener('mouseleave', () => {
-    videoAnonymous.pause();
-});
-
-
-try{
-    if (localStorage.getItem('theme') === 'dark'){
-        document.querySelector('html').classList.add('dark');
-        videoApp.src = './static/One click to start_dark.mp4';
-        videoExchange.src = './static/Real-timeexchange_dark.mp4'
-        videoSendCoins.src = './static/Send coins to users_dark.mp4'
+document.querySelector('.header__switch-theme').addEventListener('click', (event) => {
+    event.preventDefault();
+    if (localStorage.getItem('theme') === 'dark') {
+        localStorage.removeItem('theme');
+    } else {
+        localStorage.setItem('theme', 'dark');
     }
-} catch (err) {}
+    addDarkClass();
+});
+
+function addDarkClass() {
+    try {
+        const isDarkTheme = localStorage.getItem('theme') === 'dark';
+        const htmlElement = document.querySelector('html');
+        const darkVideoSuffix = isDarkTheme ? '_dark' : '';
+        
+        htmlElement.classList.toggle('dark', isDarkTheme);
+        
+        videoApp.src = `./static/One-click-to-start${darkVideoSuffix}.webm`;
+        videoExchange.src = `./static/Real-timeexchange${darkVideoSuffix}.webm`;
+        videoSendCoins.src = `./static/Send-coins-to-users${darkVideoSuffix}.webm`;
+        switchButton.src = `./static/${isDarkTheme ? 'dark' : 'light'}.svg`;
+    } catch (err) {}
+}
+
+addDarkClass();
